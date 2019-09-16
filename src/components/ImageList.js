8cdb92cb22c06506c04c5f97d2e88b4775a1f92d
mariel-delagarza <mariel.code@gmail.com>
2019-09-15 20:43:19 -0400

    Add key property to img

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,7 +3,7 @@ import { tsPropertySignature } from '@babel/types';
 
 const ImageList = (props) => {
   const images = props.images.map((image) => {
-    return <img src={image.urls.regular} />
+    return <img key={image.id} src={image.urls.regular} />
   });
 
   return <div>{images}</div>;
