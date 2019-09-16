c5dfcd832a2a723dcce4489cddfd617af7262264
mariel-delagarza <mariel.code@gmail.com>
2019-09-15 20:28:32 -0400

    Replace console.log with an arrow function to return photos and save them as const which can be returned in a div

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { tsPropertySignature } from '@babel/types';
 
-const ImageList = () => {
-  return <div>Imageist</div>;
+const ImageList = (props) => {
+  const images = props.images.map((image) => {
+    return <img src={image.urls.regular} />
+  });
+
+  return <div>{images}</div>;
 };
 
 export default ImageList;
